ec19f9639963be713ad7223c2b704790bd83e923
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2024-12-29 18:25:58 +0100

    //Add a bonus immediately: if an employee worked more than 160 hours, //they receive double the hourly rate for every additional hour.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 //- Calculate the payout for each employee
 //and write it into the span.salary field for each employee.
+
+//Add a bonus immediately: if an employee worked more than 160 hours,
+//they receive double the hourly rate for every additional hour.
+
 const workedHours = document.querySelectorAll(".czas");
 const rate = document.querySelectorAll(".stawka");
 const calculate = document.getElementById("oblicz");
@@ -12,12 +16,24 @@ function getValuesOfInputs(theInputs) {
   }
   return anInputNumberArray;
 }
+
+function giveBonus(theHourWorked, theRate) {
+  let bonus;
+  if (theHourWorked > 160) {
+    let base = 160 * theRate;
+    bonus = theRate * 2 * (theHourWorked - 160);
+    return base + bonus;
+  } else {
+    return theHourWorked * theRate;
+  }
+}
+
 function calculatePayout() {
   const AllHoursWorked = getValuesOfInputs(workedHours);
   const AllRates = getValuesOfInputs(rate);
   let result = [];
   for (let i = 0; i < AllHoursWorked.length; i++) {
-    result.push(AllHoursWorked[i] * AllRates[i]);
+    result.push(giveBonus(AllHoursWorked[i], AllRates[i]));
   }
   return result;
 }
